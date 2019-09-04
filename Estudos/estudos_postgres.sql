cb2c5cf5d74d510651ac02a422c2001378cec4ad
Natan Nascimento <natanascimentom@outlook.com>
2019-09-04 11:26:37 -0300

    Estudos do uso de Querys no Postgresql

---
+++ Estudos/estudos_postgres.sql
@@ -0,0 +1,26 @@
+
+****REMOVER UMA COLUNA NO POSTGRESQL****
+ALTER TABLE ***nomedatabela*** DROP COLUMN ***nomedacoluna***
+
+****RENOMEANDO A COLUNA*****
+ALTER TABLE ***nomedatabela*** RENAME COLUMN ***nomedacoluna*** TO ***novonomedacoluna***
+
+****ATUALIZANDO VALORES*****
+UPDATE **NOMEDATABELA**
+SET **NOMEDACOLUNA** = 'valor'
+WHERE COD_REF = **codigo**;
+
+****RENOMEAR TABELA****
+ALTER TABLE ***tabelpararenomear*** RENAME TO ***novo_nome***;
+
+
+****CRIANDO UMA NOVA TABELA****
+CREATE TABLE **NOMEDATABELA** (
+ **NOMEDACOLUNA** integer CONSTRAINT pk_**NOMEDACOLUNA** PRIMARY KEY,
+ **NOMEDACOLUNA** varchar(30) NOT NULL, 
+ **NOMEDACOLUNA** varchar(40) NOT NULL,
+ **NOMEDACOLUNA** date
+);
+
+****EXCLUINDO UMA TABELA****
+DROP TABLE ***nome da tabela***
