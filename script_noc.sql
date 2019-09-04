a7dfcfef3ff5cfe6d7b1738d87f8330260562112
Natan Nascimento <natanascimentom@outlook.com>
2019-09-04 10:20:37 -0300

    Alteração na Composição dos ID's
    Alteração nos ID's das Unidades, os mesmos estão relacionados as Instituições de Ensino

---
+++ script_noc.sql
@@ -0,0 +1,156 @@
+/* Alterando as Informações de algumas colunas do DW */
+
+/*TABELA UNIDADES*/
+
+/*UNIT SERGIPE*/
+
+UPDATE unidades
+SET id_ies = '1'
+WHERE id_unidade = 100;
+
+UPDATE unidades
+SET id_ies = '1'
+WHERE id_unidade = 101;
+
+UPDATE unidades
+SET id_ies = '1'
+WHERE id_unidade = 102;
+
+UPDATE unidades
+SET id_ies = '1'
+WHERE id_unidade = 103;
+
+UPDATE unidades
+SET id_ies = '1'
+WHERE id_unidade = 104;
+
+UPDATE unidades
+SET id_ies = '1'
+WHERE id_unidade = 105;
+
+UPDATE unidades
+SET id_ies = '1'
+WHERE id_unidade = 108;
+
+UPDATE unidades
+SET id_ies = '1'
+WHERE id_unidade = 109;
+
+UPDATE unidades
+SET id_ies = '1'
+WHERE id_unidade = 110;
+
+UPDATE unidades
+SET id_ies = '1'
+WHERE id_unidade = 111;
+
+UPDATE unidades
+SET id_ies = '1'
+WHERE id_unidade = 112;
+
+UPDATE unidades
+SET id_ies = '1'
+WHERE id_unidade = 113;
+
+UPDATE unidades
+SET id_ies = '1'
+WHERE id_unidade = 114;
+
+UPDATE unidades
+SET id_ies = '1'
+WHERE id_unidade = 115;
+
+UPDATE unidades
+SET id_ies = '1'
+WHERE id_unidade = 116;
+
+UPDATE unidades
+SET id_ies = '1'
+WHERE id_unidade = 117;
+
+UPDATE unidades
+SET id_ies = '1'
+WHERE id_unidade = 120;
+
+UPDATE unidades
+SET id_ies = '1'
+WHERE id_unidade = 122;
+
+UPDATE unidades
+SET id_ies = '1'
+WHERE id_unidade = 124;
+
+UPDATE unidades
+SET id_ies = '1'
+WHERE id_unidade = 125;
+
+UPDATE unidades
+SET id_ies = '1'
+WHERE id_unidade = 126;
+
+UPDATE unidades
+SET id_ies = '1'
+WHERE id_unidade = 129;
+
+UPDATE unidades
+SET id_ies = '1'
+WHERE id_unidade = 130;
+
+UPDATE unidades
+SET id_ies = '1'
+WHERE id_unidade = 131;
+
+UPDATE unidades
+SET id_ies = '1'
+WHERE id_unidade = 132;
+
+UPDATE unidades
+SET id_ies = '1'
+WHERE id_unidade = 133;
+
+UPDATE unidades
+SET id_ies = '1'
+WHERE id_unidade = 134;
+
+UPDATE unidades
+SET id_ies = '1'
+WHERE id_unidade = 135;
+
+/*UNIT ALAGOAS*/
+UPDATE unidades
+SET id_ies = '2'
+WHERE id_unidade = 106;
+
+UPDATE unidades
+SET id_ies = '2'
+WHERE id_unidade = 107;
+
+/*UNIT PERNAMBUCO*/
+UPDATE unidades
+SET id_ies = '3'
+WHERE id_unidade = 118;
+
+UPDATE unidades
+SET id_ies = '3'
+WHERE id_unidade = 119;
+
+UPDATE unidades
+SET id_ies = '3'
+WHERE id_unidade = 121;
+
+/*FACULDADE SÃO LUIS*/
+UPDATE unidades
+SET id_ies = '4'
+WHERE id_unidade = 127;
+
+UPDATE unidades
+SET id_ies = '4'
+WHERE id_unidade = 128;
+
+/*FITS*/
+UPDATE unidades
+SET id_ies = '5'
+WHERE id_unidade = 123;
+
+
+
