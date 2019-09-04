ecc7834992a67004a9819b5d9a6862bca100d50e
Natan Nascimento <natanascimentom@outlook.com>
2019-09-04 11:25:40 -0300

    Alterando Estrutura da antiga tabela CIDADES_ESTADOS para IES

--- DW NOC/att_unidades.sql
+++ DW NOC/att_unidades.sql
@@ -4,151 +4,151 @@
 
 /*UNIT SERGIPE*/
 
-UPDATE unidades
+UPDATE public.unidades
 SET id_ies = '1'
 WHERE id_unidade = 100;
 
-UPDATE unidades
+UPDATE public.unidades
 SET id_ies = '1'
 WHERE id_unidade = 101;
 
-UPDATE unidades
+UPDATE public.unidades
 SET id_ies = '1'
 WHERE id_unidade = 102;
 
-UPDATE unidades
+UPDATE public.unidades
 SET id_ies = '1'
 WHERE id_unidade = 103;
 
-UPDATE unidades
+UPDATE public.unidades
 SET id_ies = '1'
 WHERE id_unidade = 104;
 
-UPDATE unidades
+UPDATE public.unidades
 SET id_ies = '1'
 WHERE id_unidade = 105;
 
-UPDATE unidades
+UPDATE public.unidades
 SET id_ies = '1'
 WHERE id_unidade = 108;
 
-UPDATE unidades
+UPDATE public.unidades
 SET id_ies = '1'
 WHERE id_unidade = 109;
 
-UPDATE unidades
+UPDATE public.unidades
 SET id_ies = '1'
 WHERE id_unidade = 110;
 
-UPDATE unidades
+UPDATE public.unidades
 SET id_ies = '1'
 WHERE id_unidade = 111;
 
-UPDATE unidades
+UPDATE public.unidades
 SET id_ies = '1'
 WHERE id_unidade = 112;
 
-UPDATE unidades
+UPDATE public.unidades
 SET id_ies = '1'
 WHERE id_unidade = 113;
 
-UPDATE unidades
+UPDATE public.unidades
 SET id_ies = '1'
 WHERE id_unidade = 114;
 
-UPDATE unidades
+UPDATE public.unidades
 SET id_ies = '1'
 WHERE id_unidade = 115;
 
-UPDATE unidades
+UPDATE public.unidades
 SET id_ies = '1'
 WHERE id_unidade = 116;
 
-UPDATE unidades
+UPDATE public.unidades
 SET id_ies = '1'
 WHERE id_unidade = 117;
 
-UPDATE unidades
+UPDATE public.unidades
 SET id_ies = '1'
 WHERE id_unidade = 120;
 
-UPDATE unidades
+UPDATE public.unidades
 SET id_ies = '1'
 WHERE id_unidade = 122;
 
-UPDATE unidades
+UPDATE public.unidades
 SET id_ies = '1'
 WHERE id_unidade = 124;
 
-UPDATE unidades
+UPDATE public.unidades
 SET id_ies = '1'
 WHERE id_unidade = 125;
 
-UPDATE unidades
+UPDATE public.unidades
 SET id_ies = '1'
 WHERE id_unidade = 126;
 
-UPDATE unidades
+UPDATE public.unidades
 SET id_ies = '1'
 WHERE id_unidade = 129;
 
-UPDATE unidades
+UPDATE public.unidades
 SET id_ies = '1'
 WHERE id_unidade = 130;
 
-UPDATE unidades
+UPDATE public.unidades
 SET id_ies = '1'
 WHERE id_unidade = 131;
 
-UPDATE unidades
+UPDATE public.unidades
 SET id_ies = '1'
 WHERE id_unidade = 132;
 
-UPDATE unidades
+UPDATE public.unidades
 SET id_ies = '1'
 WHERE id_unidade = 133;
 
-UPDATE unidades
+UPDATE public.unidades
 SET id_ies = '1'
 WHERE id_unidade = 134;
 
-UPDATE unidades
+UPDATE public.unidades
 SET id_ies = '1'
 WHERE id_unidade = 135;
 
 /*UNIT ALAGOAS*/
-UPDATE unidades
+UPDATE public.unidades
 SET id_ies = '2'
 WHERE id_unidade = 106;
 
-UPDATE unidades
+UPDATE public.unidades
 SET id_ies = '2'
 WHERE id_unidade = 107;
 
 /*UNIT PERNAMBUCO*/
-UPDATE unidades
+UPDATE public.unidades
 SET id_ies = '3'
 WHERE id_unidade = 118;
 
-UPDATE unidades
+UPDATE public.unidades
 SET id_ies = '3'
 WHERE id_unidade = 119;
 
-UPDATE unidades
+UPDATE public.unidades
 SET id_ies = '3'
 WHERE id_unidade = 121;
 
 /*FACULDADE SÃO LUIS*/
-UPDATE unidades
+UPDATE public.unidades
 SET id_ies = '4'
 WHERE id_unidade = 127;
 
-UPDATE unidades
+UPDATE public.unidades
 SET id_ies = '4'
 WHERE id_unidade = 128;
 
 /*FITS*/
-UPDATE unidades
+UPDATE public.unidades
 SET id_ies = '5'
 WHERE id_unidade = 123;
 
